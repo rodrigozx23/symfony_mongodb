26b394f015eb382c2e5f652cae6a82a3466330c9
Rodrigo Bejar Chacon <rodrigozx23@gmail.com>
2018-05-02 21:46:17 -0500

    Ultimos cambios, creacion de la entidad, y de una vista simple, se obtiene un error de
    Attempted to load class "ObjectID" from namespace "MongoDB\BSON". Did you forget a "use" statement for another namespace?

---
+++ src/Controller/DataController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: usuario
+ * Date: 02/05/2018
+ * Time: 20:40
+ */
+
+namespace App\Controller;
+
+
+class dataController
+{
+
+}
---
+++ src/Document/dataForm.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: usuario
+ * Date: 02/05/2018
+ * Time: 21:30
+ */
+
+namespace App\Document;
+
+
+class dataForm
+{
+
+}
